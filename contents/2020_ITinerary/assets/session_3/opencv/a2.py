51874e792a953761e20f7c5a8b9063aea64b4b2b
EunSeong-Park <dmstjd517@unist.ac.kr>
2020-11-30 20:57:39 +0900

    ad

--- contents/2020_ITinerary/assets/session_3/opencv/a2.py
+++ contents/2020_ITinerary/assets/session_3/opencv/a2.py
@@ -1,6 +1,41 @@
 import cv2
+import numpy as np
+import time
+import math
 
-img = cv2.imread("mycutecat.png", cv2.IMREAD_GRAYSCALE)
 
-img_new = cv2.resize()
-cv2.warpAffine()
+def get_point_on_circle(center, radius, theta):
+    return (int(radius * math.cos(theta) + center[0]),
+            int(radius * math.sin(theta) + center[1]))
+
+
+
+img = np.zeros((600, 600,3), np.uint8)
+img[:, :] = [255, 255, 255]
+
+
+
+while True:
+    tmp_s = (time.time() % 628) * 3
+    tmp_m = tmp_s / 60
+    tmp_h = tmp_s / 3600
+
+    img = np.zeros((600, 600, 3), np.uint8)
+    img[:, :] = [255, 255, 255]
+
+    img = cv2.circle(img, (300, 300), 270, (0, 0, 0), thickness=8)
+    img = cv2.circle(img, (300, 300), 5, (0, 0, 0), thickness=-1)
+    img_2 = cv2.line(img, (300, 300),
+                     get_point_on_circle((300, 300), 250, tmp_s), (0, 0, 255),
+                     thickness=2)
+    img_2 = cv2.line(img, (300, 300),
+                     get_point_on_circle((300, 300), 180, tmp_m), (0, 0, 0),
+                     thickness=4)
+    img_2 = cv2.line(img, (300, 300),
+                     get_point_on_circle((300, 300), 100, tmp_h), (0, 0, 0),
+                     thickness=9)
+
+    cv2.imshow("window", img_2)
+    if cv2.waitKey(1000 // 30) == 27:
+        break
+
